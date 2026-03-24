82d3b102b93a89857a06d93692617113d32ffbca
Alfonso Reyes <alfonsoreyes656@gmail.com>
2026-03-24 16:48:01 +0100

    cambios añadidos

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://xubpjmdepmppowolyay.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1YnBqZG1kZXBtcHBvd29seWF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxOTI4MDAsImV4cCI6MjA4OTc2ODgwMH0.-0VcUMf9NMVxRjuy6iENnnOOsSnh16U6dnwmCao99Fs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
