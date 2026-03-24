b6e1e6ca25505db0f4c358de3600099fe52d4b69
Alfonso Reyes <alfonsoreyes656@gmail.com>
2026-03-24 19:17:20 +0100

    ajustes realizados en mapa footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,9 +1,12 @@
 import { NavLink } from "react-router-dom";
-import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaPhoneAlt, FaWhatsapp } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaMapMarkerAlt, FaPhoneAlt, FaWhatsapp, FaDirections } from 'react-icons/fa';
 import "../styles/Footer.css";
 
 function Footer() {
   const manejarClick = (e) => e.preventDefault();
+
+  // URL directa a Google Maps para Taller Motors
+  const googleMapsUrl = "https://www.google.com/maps/dir/?api=1&destination=Av.+de+Italia,+27,+14550+Montilla,+Córdoba";
 
   return (
     <footer className="footer-principal">
@@ -40,12 +43,12 @@ function Footer() {
           </div>
         </div>
 
-        {/* Mapa con Iframe de Google */}
+        {/* Mapa con Iframe y Botón de Acción */}
         <div className="footer-ubicacion">
           <h4>Ubicación</h4>
           <div className="mapa-simulado-footer">
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.427387405232!2d-4.639892623467616!3d37.58007497203417!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6d39695d732891%3A0x7d07943477103e65!2sAv.%20de%20Italia%2C%2027%2C%2014550%20Montilla%2C%20C%C3%B3rdoba!5e0!3m2!1ses!2ses!4v1711310000000!5m2!1ses!2ses" 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3161.474643194511!2d-4.639892623455!3d37.579475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6d17e3f8b0e8e7%3A0x6b1f2e3f8b0e8e7!2sAv.%20de%20Italia%2C%2027%2C%2014550%20Montilla%2C%20C%C3%B3rdoba!5e0!3m2!1ses!2ses!4v1711310000000!5m2!1ses!2ses" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
@@ -55,6 +58,14 @@ function Footer() {
               title="Ubicación Taller Motors"
             ></iframe>
           </div>
+          <a 
+            href={googleMapsUrl} 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="btn-como-llegar"
+          >
+            <FaDirections /> CÓMO LLEGAR
+          </a>
         </div>
 
       </div>
